25ea0f8532aae0315f3b6a42d7c6c6d37caae773
Al <alexseier12345@gmail..com>
2021-06-06 18:33:26 +0300

    1 version

--- lesson1/loadScript/js/load.js
+++ lesson1/loadScript/js/load.js
@@ -1,8 +1,28 @@
-function loadScript(url, callback) {
-    const element = document.createElement("script");
-    element.type = "text/javascript";
-    element.src = url;
-    element.onload = callback;
-
-    document.body.appendChild(element);
+function loadScript(url, callback = null) {
+    if (typeof url === 'string') {
+        const element = document.createElement("script");
+        element.type = "text/javascript";
+        element.src = url;
+        element.onload = callback;
+        document.body.appendChild(element);
+    } else if (Array.isArray(url)) {
+        url.forEach(elem=> loadScript(elem));
+        callback()
+    }
 }
+
+// loadScript(["js/common.js", 'ascsac.js'], () => {
+//     loadScript("https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js", () => {
+//         console.log("timer.js ")
+//     })
+// })
+
+// let index = 0
+// if(index < url.length) {
+//     index++;
+//     const element = document.createElement("script");
+//     element.type = "text/javascript";
+//     element.src = loadScript(url[index]);
+//     element.onload = callback;
+//     document.body.appendChild(element);
+// } callback()
--- lesson1/loadScript/js/timer.js
+++ lesson1/loadScript/js/timer.js
@@ -1,4 +1,4 @@
-loadScript("js/common.js", () => {
+loadScript(["js/common.js", "https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.js"], () => {
     loadScript("https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js", () => {
         console.log("timer.js ")
     })
