4b6fa901f40e90cae935dfb8c983cc08605f80b7
Al <alexseier12345@gmail..com>
2021-06-07 00:14:47 +0300

    update

--- lesson1/loadScript/js/load.js
+++ lesson1/loadScript/js/load.js
@@ -1,32 +1,42 @@
-let index = 0;
-
-function counter() {
-    index++
-}
-
-let buffer = [];
+let _index = 0;
+let _buffer = [];
 
 function loadScript(url, callback = null) {
     if (typeof url === 'string') {
-        if (!buffer.find(elem => elem === url)) {
-            buffer.push(url);
-            const element = document.createElement("script");
-            element.type = "text/javascript";
-            element.src = url;
-            if (!callback) {
-                element.onload = counter;
-            } else element.onload = callback;
-            document.body.appendChild(element);
+        if (!_findInBuffer(url)) _loadArgString(url, callback);
+    } else if (Array.isArray(url)) _loadArgArray(url, callback);
+}
+
+function _counter() {
+    _index++;
+}
+
+function _loadArgString(url, callback) {
+    _buffer.push(url);
+    const element = document.createElement("script");
+    element.type = "text/javascript";
+    element.src = url;
+
+    if (!callback) {
+        element.onload = _counter;
+    } else element.onload = callback;
+
+    document.body.appendChild(element);
+}
+
+function _loadArgArray(url, callback) {
+    url.forEach(elem => loadScript(elem));
+
+    const check = setInterval(() => {
+        if (_index === url.length) {
+            callback();
+            _index = 0;
+            clearInterval(check);
         }
-    } else if (Array.isArray(url)) {
-        url.forEach(elem => loadScript(elem));
-        const check = setInterval(() => {
-            if (index === url.length) {
-                callback()
-                index = 0
-                clearInterval(check)
-            }
-        })
-    }
+    })
+}
+
+function _findInBuffer(url) {
+    return _buffer.find(elem => elem === url);
 }
 
