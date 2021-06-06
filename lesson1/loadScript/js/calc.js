737b9b0a11272c1c65777f2aa9afe4f473b691f7
Al <alexseier12345@gmail..com>
2021-06-06 23:57:17 +0300

    add buffer to loadScript

--- lesson1/loadScript/js/calc.js
+++ lesson1/loadScript/js/calc.js
@@ -1,5 +1,7 @@
-loadScript(["js/common.js", "https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.js", 'js/calc.js'], () => {
+loadScript(["js/common.js", "https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.js"], () => {
     loadScript("https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js", () => {
         console.log("timer.js ")
     })
 })
+
+console.log('calc.js')
--- lesson1/loadScript/js/load.js
+++ lesson1/loadScript/js/load.js
@@ -1,20 +1,26 @@
 let index = 0;
+
 function counter() {
     index++
 }
 
+let buffer = [];
+
 function loadScript(url, callback = null) {
     if (typeof url === 'string') {
-        const element = document.createElement("script");
-        element.type = "text/javascript";
-        element.src = url;
-        if(!callback) {
-            element.onload = counter;
-        } else element.onload = callback;
-        document.body.appendChild(element);
+        if (!buffer.find(elem => elem === url)) {
+            buffer.push(url);
+            const element = document.createElement("script");
+            element.type = "text/javascript";
+            element.src = url;
+            if (!callback) {
+                element.onload = counter;
+            } else element.onload = callback;
+            document.body.appendChild(element);
+        }
     } else if (Array.isArray(url)) {
         url.forEach(elem => loadScript(elem));
-       const check = setInterval(() => {
+        const check = setInterval(() => {
             if (index === url.length) {
                 callback()
                 index = 0
