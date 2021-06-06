9ad7df32f76bca8e8d893bff8118b82a0655c172
Al <alexseier12345@gmail..com>
2021-06-06 20:47:54 +0300

    2 version

--- lesson1/loadScript/js/load.js
+++ lesson1/loadScript/js/load.js
@@ -1,28 +1,26 @@
+let index = 0;
+function counter() {
+    index++
+}
+
 function loadScript(url, callback = null) {
     if (typeof url === 'string') {
         const element = document.createElement("script");
         element.type = "text/javascript";
         element.src = url;
-        element.onload = callback;
+        if(!callback) {
+            element.onload = counter;
+        } else element.onload = callback;
         document.body.appendChild(element);
     } else if (Array.isArray(url)) {
-        url.forEach(elem=> loadScript(elem));
-        callback()
+        url.forEach(elem => loadScript(elem));
+       const check = setInterval(() => {
+            if (index === url.length) {
+                callback()
+                index = 0
+                clearInterval(check)
+            }
+        })
     }
 }
 
-// loadScript(["js/common.js", 'ascsac.js'], () => {
-//     loadScript("https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js", () => {
-//         console.log("timer.js ")
-//     })
-// })
-
-// let index = 0
-// if(index < url.length) {
-//     index++;
-//     const element = document.createElement("script");
-//     element.type = "text/javascript";
-//     element.src = loadScript(url[index]);
-//     element.onload = callback;
-//     document.body.appendChild(element);
-// } callback()
